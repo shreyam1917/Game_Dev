e7e6d5151a3e058425ea924916070729c86748d8
shreyam1917 <shreyam.111.official@gmail.com>
2023-01-10 10:05:00 +0530

    Day 39

---
+++ Shooting.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    /*public GameObject Bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+     public void Shoot()
+    {
+        var clone = Instantiate(Bullet, transform.position,transform.rotation);
+        Destroy(clone, 10f);
+    }*/
+}
